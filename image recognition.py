ff620190ffda530c091da68bd0b2194f458e4e49
dlrlgugu <hckguja@gmail.com>
2018-05-30 14:27:50 -0700

    tried training own data but not worked so well; Signed-off-by: dlrlgugu <hckguja@gmail.com>

---
+++ image recognition.py
@@ -0,0 +1,184 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from own_data_preprocessing import own_data_preprocessing
+import tensorflow as tf
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y = own_data_preprocessing()
+
+#index = 25
+#plt.imshow(train_set_x_orig[index])
+#plt.show()
+#print(str(train_set_y[:,index]) + "it's a "+classes[np.squeeze(train_set_y[:,index])].decode("utf-8") )
+
+print(train_set_x_orig.shape) #(209, 64, 64, 3)
+print(train_set_y.shape) #(1, 209)
+print(test_set_x_orig.shape)#(50, 64, 64, 3)
+print(test_set_y.shape)#(1, 50)
+
+
+print(train_set_x_orig.shape[0])
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+#dimensions and shapes of problem
+#reshape
+#standardize.
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T,X)+b)                                    # compute activation
+    cost = -1/m*np.sum((Y*np.log(A)+(1-Y)*np.log(1-A)))                                 # compute cost
+    ### END CODE HERE ###
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dw = (1/m)*np.dot(X,(A - Y).T)
+    db = 1/m*np.sum(A-Y)
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ### 
+        grads, cost = propagate(w,b,X,Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w-learning_rate*dw
+        b = b-learning_rate*db
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+
+    A=sigmoid(np.dot(w.T,X)+b)
+
+    for i in range(A.shape[1]):
+        if A[0,i] > 0.5:
+            Y_prediction[0,i] = 1
+        else :
+            Y_prediction[0,i] = 0
+
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    ### END CODE HERE ###
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+
+with tf.device('/gpu:0'):
+    d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 200, learning_rate = 0.005, print_cost = True)
+
+
+test_image = ["cat1.jpg","cat2.jpg","cat3.jpg"]
+for i in test_image:
+    image = np.array(ndimage.imread(i,flatten=False))
+    my_image = scipy.misc.imresize(image,size=(64,64)).reshape((1,64*64*3)).T
+    my_predicted_image = predict(d["w"],d["b"],my_image)
+
+    plt.imshow(image)
+    print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" )
+    print (int(np.squeeze(my_predicted_image)) )
+
